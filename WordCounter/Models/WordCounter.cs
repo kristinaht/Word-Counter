05748fa8cc8495a894226ea6f68884f37507841c
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-06 09:22:06 -0800

    add WordCounter class

---
+++ WordCounter/Models/WordCounter.cs
@@ -0,0 +1,14 @@
+namespace WordCounter.Models
+{
+  public class WordCounter
+  {
+    public string Word { get; set; }
+    public string Sentence { get; set; }
+
+    public WordCounter(string word, string sentence)
+    {
+      
+    }
+
+  }
+}
