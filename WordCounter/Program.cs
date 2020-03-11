2effe3a7c150e25daa378ced9fcfcf20ded6bcd9
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-10 17:33:46 -0700

    modify basic UI

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -8,15 +8,17 @@ namespace WordCounter
   {
     public static void Main()
     {
-      Console.WriteLine("enter word");
-      string word = Console.ReadLine();
-      Console.WriteLine("enter sentence");
-      string sentence = Console.ReadLine();
+      Console.WriteLine("Welcome to Word Checker!");
+      Console.WriteLine("Enter a word");
+      string wordMix = Console.ReadLine();
+      string word = wordMix.ToLower();
+      Console.WriteLine("Enter a sentence");
+      string sentenceMix = Console.ReadLine();
+      string sentence = sentenceMix.ToLower();
       Checker newChecker = new Checker(word, sentence);
       Console.WriteLine(newChecker.Sentence);
       int wordCount = newChecker.WordCount(sentence);
-      Console.WriteLine(wordCount);
-      // Console.WriteLine(newChecker.WordCount(array));
+      Console.WriteLine("Word '" + word + "'" + "appears " + wordCount + " time/s in the '" + sentence + "' sentence.");
 
       
     }
